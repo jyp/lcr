8a6306a08066aa6b17cba1d1f278cb359d6b6c6a
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-01-27 21:29:53 +0100

    add GPL

--- lcr.el
+++ lcr.el
@@ -13,6 +13,21 @@
 ;; With parts copied from the work of Daniel Colascione
 ;; <dancol@dancol.org> on generators.
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 ;;; Commentary:
 
 ;; We call a lightweight coroutine (or `lcr' for short) a function
